60b9935b5a04b4c839732843627a5dd44a089132
Konrad <konrad.kornak9@gmail.com>
2024-06-12 13:24:20 +0200

    first commit

---
+++ src/app/account/page.tsx
@@ -0,0 +1,107 @@
+"use client";
+import { useUserAuth } from "@/lib/userAuth";
+import { useState, useEffect } from "react";
+import { supabase } from "../../lib/supabase";
+import { useUser } from "../../../hooks/useUser";
+import Button from "@/components/Button";
+
+function UsernameCheck() {
+  const [username, setUsername] = useState("");
+  const [isUsernameMissing, setIsUsernameMissing] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const { user } = useUser();
+
+  useEffect(() => {
+    checkUsername();
+  }, [user]);
+
+  async function checkUsername() {
+    try {
+      const { data, error } = await supabase
+        .from("users")
+        .select("username")
+        .eq("id", user?.id);
+
+      if (error) {
+        throw error;
+      }
+
+      if (!data || !data.length || !data[0].username) {
+        setIsUsernameMissing(true);
+        setShowModal(true);
+      } else {
+        setUsername(data[0].username);
+      }
+    } catch (error) {
+      console.error("Error checking username:", error);
+    }
+  }
+
+  const handleAddUsername = async () => {
+    try {
+      const { data, error } = await supabase
+        .from("users")
+        .update({ username })
+        .eq("id", user?.id);
+
+      setIsUsernameMissing(false);
+      setShowModal(false);
+      checkUsername();
+      if (error) {
+        throw error;
+      }
+
+      if (data) {
+        // setIsUsernameMissing(false);
+        // setShowModal(false);
+        // checkUsername();
+      }
+    } catch (error) {
+      console.error("Error adding username:", error);
+    }
+  };
+
+  return (
+    <div className="max-w-md mx-auto p-8">
+      {showModal && (
+        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
+          <div className="bg-gray-100 p-12 rounded-2xl">
+            <p className="font-bold text-xl">Proszę dodaj swój nick:</p>
+            <input
+              type="text"
+              placeholder="Wpisz tutaj"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="mt-4 mb-1 p-2 border rounded flex border-gray-400 focus:border-black"
+              style={{
+                outline: "none",
+                boxShadow: "0 0 3px rgba(0, 0, 0, 0.5)",
+              }}
+            />
+            <Button
+              onClick={handleAddUsername}
+              className="mt-4 bg-black text-white p-2 rounded"
+            >
+              Dodaj nick
+            </Button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default function Home() {
+  useUserAuth();
+  const { user } = useUser();
+  useEffect(() => {
+  }, [user]);
+
+
+  return (
+    <div className="flex bg-gray-100 dark:bg-gray-900 lg:p-24 md:p-12 p-8 grid lg:grid-cols-2 grid-cols-1 lg:divide-x">
+
+    </div>
+  );
+}
+
---
+++ src/components/ui/Navbar.tsx
@@ -0,0 +1,116 @@
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+import useAuthModal from "../../../hooks/useAuthModal";
+import Button from "../Button";
+import { useRouter } from "next/navigation";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useUser } from "../../../hooks/useUser";
+import { toast } from "react-hot-toast";
+import { FaUserAlt } from "react-icons/fa";
+import { supabase } from "@/lib/supabase";
+import { useEffect } from "react";
+
+function isScreenSizeGreaterThan1000() {
+  return window.innerWidth > 1000;
+}
+
+const Navbar = () => {
+  const { user } = useUser();
+  const [username, setUsername] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  const authModal = useAuthModal();
+  const supabaseClient = useSupabaseClient();
+
+
+  const findUser = async () => {
+    try {
+      setLoading(true);
+
+      if (user) {
+        const { data, error } = await supabase
+          .from("users")
+          .select("id, username")
+          .eq("id", user.id)
+          .single();
+
+        if (error) {
+          throw error;
+        }
+
+        if (data) {
+          setUsername(data.username || null);
+        }
+      } else {
+        // Throw an error if the user object is not available.
+        throw new Error("User information is not available.");
+      }
+    } catch (error) {
+      console.error("Error finding user:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    findUser();
+  }, [user]);
+
+  const handleLogout = async () => {
+    const { error } = await supabaseClient.auth.signOut();
+    //player.reset();
+    router.refresh();
+    router.push("/");
+    if (error) {
+      toast.error(error.message);
+    }
+  };
+
+
+  return (
+    <div className="sticky inset-x-0 top-0 bg-white dark:bg-gray-950 z-[20] h-fit border-b border-zinc-300">
+      <div className="flex items-center justify-between py-2 px-8 mx-auto max-w-7xl">
+        <button
+          onClick={() => router.push("/")}
+          className="rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white"
+        >
+          Quiz_app
+        </button>
+
+        {user ? (
+          <div className="flex gap-x-4 items-center">
+            {isScreenSizeGreaterThan1000() && (
+              <div className="email">
+                {loading ? "Loading..." : username || user.email}
+              </div>
+            )}
+            <Button
+              onClick={() => router.push("/account")}
+              className="bg-white"
+            >
+              <FaUserAlt />
+            </Button>
+            <Button
+              onClick={handleLogout}
+              className="bg-white rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white"
+            >
+              Wyloguj
+            </Button>
+
+
+          </div>
+        ) : (
+          <button
+            onClick={authModal.onOpen}
+            className="rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white ml-4"
+          >
+            Zaloguj
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
